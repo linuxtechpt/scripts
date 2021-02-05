3922c6d2180a1580163131924b20b1cb88349615
Rubem Mota <rubemmota89@gmail.com>
2021-02-05 20:46:53 +0000

    Added basic support for fedora
    Added basic support for fedora.
    Refactored code to avoid lots of replication
    
    Added linter to bash code

--- installation_protonbridge.sh
+++ installation_protonbridge.sh
@@ -2,85 +2,87 @@
 #Data: 30-01-2021
 # Instalação e configuração do protonbridge
 
-echo "1 | Verificar o pacote protonmail-bridge no sistema..."
-which protonmail-bridge &> /dev/null 
-if [ $? -eq 0 ] 
-  then
-	  echo "Pacote instalado no sistema"
-  else
-	  echo "Queres instalar o pacote protonmail-bridge (y/n)?"
-	  read -r answer
-	  case "${answer}" in
-		  [Yy])
-	          echo "Download do pacote protonmail-bridge..."
-		  wget https://protonmail.com/download/protonmail-bridge_1.5.7-1_amd64.deb
-		  echo "Download concluido"
-esac
+##### Helper functions ######
+BRIDGE_VERSION=1.5.7-1
+##### Functions ######
+function persist_install() {
+	echo "Verificar o pacote $1 no sistema..."
+	if command -v "$1" &> /dev/null -eq 1; then
+		echo "Pacote instalado no sistema"
+	else
+		echo "Queres instalar o pacote $1 (y/n)?"
+		read -r answer
+		case "${answer}" in
+			[Yy]) perform_install "$2";;
+		esac
+	fi
+}
+
+function download_bridge() {
+echo "Queres fazer download da bridge (y/n)?"
+	read -r answer
+	case "${answer}" in
+		[Yy])
+		echo "Download do pacote protonmail-bridge..."
+		if command -v apt &> /dev/null ; then
+			wget -O protonmail-bridge.deb "https://protonmail.com/download/protonmail-bridge_${BRIDGE_VERSION}_amd64.deb"
+		else
+			wget -O protonmail-bridge.rpm "https://protonmail.com/download/protonmail-bridge-${BRIDGE_VERSION}.x86_64.rpm"
+		fi
+		echo "Download concluido";;
+	esac
+}
+
+function perform_install() {
+	if command -v apt &> /dev/null
+	then
+		sudo apt install -y "$1"
+		return
+	fi
+
+	if command -v dnf &> /dev/null
+	then
+		sudo dnf install -y "$1"
+		return
+	fi
+}
+######## MAIN ########
+
+echo "Verificar o pacote protonmail-bridge no sistema..."
+if command -v protonmail-bridge &> /dev/null -eq 0; then
+	echo "Bridge do Protonmail instalada"
+	exit 0
+else
+	download_bridge
 fi
-echo "2 | Verificar o pacote gdebi-core no sistema..."
-which gdebi &> /dev/null
-if [ $? -eq 0 ] 
-  then
-	  echo "Pacote já instalado no sistema!"
-  else
-	  echo "gdebi não está instalado!"
-	  echo "Queres instalar o pacote gdebi (y/n)?"
-	  read -r answer
-	  case "${answer}" in
-		  [Yy])
-	          echo "A instalar o pacote gdebi..."
-		  sudo apt install gdebi-core -y
-		  echo "Instalação o pacote gdebi concluida"
-esac
+
+# validate if is needed to install gdebi
+if command -v apt
+then
+	persist_install "gdebi-core"
 fi
-cd #HOME/Transferências/
-echo "3 | Verificar a instalação do protonmail-bridge no sistema..."
-  which protonmail-bridge &> /dev/null
-if [ $? -eq 0 ]
-  then
-          echo "Instalação do pacote protonmail-bridge concluida!"
-  else
-	  echo "3 | A instalar o pacote protonmail-bridge..."
-          sudo gdebi protonmail-bridge_1.5.7-1_amd64.deb
+
+if command -v apt; then
+	persist_install "gpg" "gnupg"
+else
+	persist_install "gpg" "gnupg2"
 fi
-echo "4 | Verificar pacote gnupg no sistema..."
-whereis gnupg &> /dev/null
-if [ $? -eq 0 ]
-  then
-	  echo "Instalação do pacote gnupg concluida!"
-  else
-	  echo "4 | A instalar o pacote gnupg..."
-	  sudo apt install gnupg -y
+
+echo "Criar nova chave..."
+gpg --gen-key
+persist_install "pass" "pass"
+
+echo "A instalar o pacote protonmail-bridge..."
+if command -v apt; then
+	sudo gdebi protonmail-bridge.deb
+else
+	sudo dnf localinstall -y protonmail-bridge.rpm
 fi
-	  echo "Criar nova chave..."
-	  gpg --gen-key
-echo "5 | A Verificar o pacote pass no sistema..."
-which pass &> /dev/null
-if [ $? -eq 0 ]
-  then
-	  echo "Pacote pass já está instalado no sistema!"
-  else
-	  echo "Pacote pass não está instalado!"
-	  echo "Queres instalar o pacote pass (y/n)?"
-	  read -r answer
-	  case "${answer}" in 
-		  [Yy])
-	          echo "A instalar o pacote pass..."
-		  sudo apt install pass -y
-esac
-fi
-which pass &> /dev/null
-if [ $? -eq 0 ]
-  then
-	  echo "Instalação do pacote pass concluida!"
-  else
-	  echo "Erro na instalação!"
-fi
-echo "5 | Gerar nova chave no pass..."
-read -p "Por favor introduza o email: "  variable_mail
-pass init $variable_mail
-pass generate email_bridge/$variable_mail \15
+
+
+echo "Gerar nova chave no pass..."
+echo "Por favor introduza o email: "
+read -r variable_mail
+pass init "$variable_mail"
+pass generate "email_bridge/$variable_mail"
 echo "Instalação e configuração do Proton_bridge concluida!"
-
-
-
